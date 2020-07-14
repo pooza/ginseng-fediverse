fb2f701a8b0a461194f8bfc17490e1c39e03fc7d
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-07-14 09:34:38 +0900

    #16

--- lib/ginseng/fediverse/service/meisskey_service.rb
+++ lib/ginseng/fediverse/service/meisskey_service.rb
@@ -4,7 +4,20 @@ module Ginseng
       include Package
 
       def announcements(params = {})
-        return nil
+        headers = params[:headers] || {}
+        headers['X-Mulukhiya'] = package_class.full_name unless mulukhiya_enable?
+        r = @http.get('/api/meta', {
+          body: {i: token}.to_json,
+          headers: headers,
+        })
+        raise Ginseng::GatewayError, "Bad response #{r.code}" unless r.code == 200
+        return r['announcements'].map do |entry|
+          {
+            'id' => Digest::SHA1.hexdigest(entry.to_json),
+            'title' => entry['title'],
+            'text' => entry['text'],
+          }
+        end
       end
 
       private
