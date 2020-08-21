0f72bee77f23caf96dd4fb32cfa40fbd4f06d96b
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-08-21 16:06:04 +0900

    #30

--- lib/ginseng/fediverse/parser.rb
+++ lib/ginseng/fediverse/parser.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Ginseng
   module Fediverse
     class Parser
--- lib/ginseng/fediverse/parser/toot_parser.rb
+++ lib/ginseng/fediverse/parser/toot_parser.rb
@@ -1,5 +1,3 @@
-require 'nokogiri'
-
 module Ginseng
   module Fediverse
     class TootParser < Parser
@@ -9,7 +7,7 @@ module Ginseng
         md = text.clone
         ['.u-url', '.hashtag'].each do |selector|
           nokogiri.css(selector).each do |link|
-            md.gsub!(link.to_s, "[#{link.inner_text}](#{link.attributes['href'].value})")
+            md.gsub!(link.to_s, "[\\#{link.inner_text}](#{link.attributes['href'].value})")
           rescue => e
             @logger.error(error: e.message, link: link.to_s)
           end
