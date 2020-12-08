e1560352a2c5d6a576cd5ea942570574bdf0f4f5
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-12-09 06:44:50 +0900

    #51

--- lib/ginseng/fediverse/refines.rb
+++ lib/ginseng/fediverse/refines.rb
@@ -9,6 +9,12 @@ module Ginseng
         def to_hashtag_base
           return Service.create_tag_base(self)
         end
+
+        def escape_toot
+          return sub(/[@#]/, '\\0 ')
+        end
+
+        alias escape_note escape_toot
       end
     end
   end
