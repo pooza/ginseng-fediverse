1035f19956c88201703e1d4a2da5fa08bee01ffd
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-01-24 11:24:04 +0900

    #66

--- lib/ginseng/fediverse/parser.rb
+++ lib/ginseng/fediverse/parser.rb
@@ -101,6 +101,8 @@ module Ginseng
       end
 
       def self.sanitize(text)
+        text = text.clone
+        text.gsub!("\n", '') if text.match?(%r{<br.*?>}) 
         text.gsub!(/\s*<br.*?>/, "\n")
         text.gsub!(%r{\s*</p.*?>}, "\n\n")
         text.gsub!(/<p.*?>/, '')
