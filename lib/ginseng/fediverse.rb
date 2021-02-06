ce5fb97668cb0f058ae4ded8c551f6af4e385e3f
pooza <tkoishi@b-shock.co.jp>
2021-02-06 11:43:45 +0900

    #68

--- lib/ginseng/fediverse.rb
+++ lib/ginseng/fediverse.rb
@@ -16,8 +16,8 @@ module Ginseng
       loader.push_dir(File.join(dir, 'lib/ginseng/fediverse'), namespace: Ginseng::Fediverse)
       return loader
     end
+
+    Bundler.require
+    loader.setup
   end
 end
-
-Bundler.require
-Ginseng::Fediverse.loader.setup
