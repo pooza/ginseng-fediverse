913e2f0a12053fb677d4d2ba4aac4215cde16a93
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2021-04-09 18:52:41 +0900

    #82

--- lib/ginseng/fediverse/service.rb
+++ lib/ginseng/fediverse/service.rb
@@ -48,7 +48,7 @@ module Ginseng
       end
 
       def upload_remote_resource(uri, params = {})
-        path = File.join(environment_class.dir, 'tmp/media', uri.to_s.adler32)
+        path = File.join(environment_class.dir, 'tmp/media', uri.to_s.adler32.to_s)
         File.write(path, http.get(uri))
         return upload(path, params)
       ensure
